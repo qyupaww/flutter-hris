997b7c8e87c44a72746727cdccdde9524167cd92
Faisal Rahman <qyupaww@gmail.com>
2026-02-18 17:42:52 +0700

    minor changes

--- lib/features/home/attendance/presentation/pages/attendance_page.dart
+++ lib/features/home/attendance/presentation/pages/attendance_page.dart
@@ -4,8 +4,7 @@ import 'package:morpheme_flutter_lite/core/components/atoms/atoms.dart';
 import 'package:morpheme_flutter_lite/core/global_variable.dart';
 import 'package:morpheme_flutter_lite/core/themes/morpheme_colors/src/morpheme_color.dart';
 import 'package:morpheme_flutter_lite/features/home/attendance/presentation/cubit/attendance_cubit.dart';
-import 'package:morpheme_flutter_lite/features/home/attendance/presentation/widgets/form_section.dart';
-import 'package:morpheme_flutter_lite/features/home/attendance/presentation/widgets/map_section.dart';
+import 'package:morpheme_flutter_lite/features/home/attendance/presentation/widgets/widgets.dart';
 
 class AttendancePage extends StatefulWidget {
   const AttendancePage({super.key});
---
+++ lib/features/home/attendance/presentation/widgets/widgets.dart
@@ -0,0 +1,2 @@
+export 'form_section.dart';
+export 'map_section.dart';
